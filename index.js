a5637fe1dc02ca59f4c30f2e56f7b39ba31165a3
progamerprop <74456772+progamerprop@users.noreply.github.com>
2020-11-15 02:42:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const ytdl = require('ytdl-core');
 const { YTSearcher } = require('ytsearcher');
 
 const searcher = new YTSearcher({
-    key: (process.env.apikey),
+    key: "AIzaSyAT_ZVOGKxun4lFdqtDe6gFg6gGoc3ye0I",
     revealed: true
 });
 
